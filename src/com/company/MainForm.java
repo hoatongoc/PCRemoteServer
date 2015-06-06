bc6ddda0cec2ebff07975b29ada3e65615b2c531
hoatongoc <hoatongoc1@yahoo.com>
2015-06-06 22:27:46 +0700

    Update keycode

--- src/com/company/MainForm.java
+++ src/com/company/MainForm.java
@@ -311,66 +311,66 @@ public class MainForm extends JFrame {
 						e.printStackTrace();
 					}
 		            try {
-						SenderDataForServerInfo data = (SenderDataForServerInfo)ois.readObject();
+						SenderData data = (SenderData)ois.readObject();
 						if(data.getCommand().equals(SocketConstant.SERVER_INFO)) {
 			                ServerInfo obj = new ServerInfo();
 			                obj.setServerName(InetAddress.getLocalHost().getHostName());
 			                obj.setServerIP(InetAddress.getLocalHost().toString());
-//			                data.setServerInfo(obj);
+			                data.setData(obj);
 			                ByteArrayOutputStream bao = new ByteArrayOutputStream(6000);
 			                ObjectOutputStream oos = new ObjectOutputStream(bao);
-			                oos.writeObject(obj);
+			                oos.writeObject(data);
 			                byte[] buf= bao.toByteArray();
 			                DatagramPacket pkg = new DatagramPacket(buf,buf.length,pk.getAddress(),pk.getPort());
 			                dsk.send(pkg);
 			            }
-//						else if(data.getCommand().equals(SocketConstant.MOUSE_CLICK)) {
-//							if(((MouseClick)data.getData()).isPress()) {
-//								int btnNum = ((MouseClick)data.getData()).getButtonNum();
-//								if(btnNum == 1) {
-//									r.mousePress(InputEvent.BUTTON1_MASK);
-//								}
-//								else if(btnNum == 2) {
-//									r.mousePress(InputEvent.BUTTON2_MASK);
-//								}
-//								else if(btnNum == 3) {
-//									r.mousePress(InputEvent.BUTTON3_MASK);
-//								}
-//							}
-//							else {
-//								int btnNum = ((MouseClick)data.getData()).getButtonNum();
-//								if(btnNum == 1) {
-//									r.mousePress(InputEvent.BUTTON1_MASK);
-//									try {
-//										Thread.sleep(20);
-//									} catch (InterruptedException e) {
-//										// TODO Auto-generated catch block
-//										e.printStackTrace();
-//									}
-//									r.mouseRelease(InputEvent.BUTTON1_MASK);
-//								}
-//								else if(btnNum == 2) {
-//									r.mousePress(InputEvent.BUTTON2_MASK);
-//									try {
-//										Thread.sleep(20);
-//									} catch (InterruptedException e) {
-//										// TODO Auto-generated catch block
-//										e.printStackTrace();
-//									}
-//									r.mouseRelease(InputEvent.BUTTON2_MASK);
-//								}
-//								else if(btnNum == 3) {
-//									r.mousePress(InputEvent.BUTTON3_MASK);
-//									try {
-//										Thread.sleep(20);
-//									} catch (InterruptedException e) {
-//										// TODO Auto-generated catch block
-//										e.printStackTrace();
-//									}
-//									r.mouseRelease(InputEvent.BUTTON3_MASK);
-//								}
-//							}
-//						}
+						else if(data.getCommand().equals(SocketConstant.MOUSE_CLICK)) {
+							if(((MouseClick)data.getData()).isPress()) {
+								int btnNum = ((MouseClick)data.getData()).getButtonNum();
+								if(btnNum == 1) {
+									r.mousePress(InputEvent.BUTTON1_MASK);
+								}
+								else if(btnNum == 2) {
+									r.mousePress(InputEvent.BUTTON2_MASK);
+								}
+								else if(btnNum == 3) {
+									r.mousePress(InputEvent.BUTTON3_MASK);
+								}
+							}
+							else {
+								int btnNum = ((MouseClick)data.getData()).getButtonNum();
+								if(btnNum == 1) {
+									r.mousePress(InputEvent.BUTTON1_MASK);
+									try {
+										Thread.sleep(20);
+									} catch (InterruptedException e) {
+										// TODO Auto-generated catch block
+										e.printStackTrace();
+									}
+									r.mouseRelease(InputEvent.BUTTON1_MASK);
+								}
+								else if(btnNum == 2) {
+									r.mousePress(InputEvent.BUTTON2_MASK);
+									try {
+										Thread.sleep(20);
+									} catch (InterruptedException e) {
+										// TODO Auto-generated catch block
+										e.printStackTrace();
+									}
+									r.mouseRelease(InputEvent.BUTTON2_MASK);
+								}
+								else if(btnNum == 3) {
+									r.mousePress(InputEvent.BUTTON3_MASK);
+									try {
+										Thread.sleep(20);
+									} catch (InterruptedException e) {
+										// TODO Auto-generated catch block
+										e.printStackTrace();
+									}
+									r.mouseRelease(InputEvent.BUTTON3_MASK);
+								}
+							}
+						}
 						
 					} catch (ClassNotFoundException e) {
 						// TODO Auto-generated catch block
