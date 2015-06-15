8b3b276678be8ae93882739c67922c05b44611d6
hoatongoc <hoatongoc1@yahoo.com>
2015-06-15 23:24:39 +0700

    update code disconnect

--- src/com/group3/pcremote/api/HandleDisconnectRequest.java
+++ src/com/group3/pcremote/api/HandleDisconnectRequest.java
@@ -3,6 +3,7 @@ package com.group3.pcremote.api;
 import java.net.InetAddress;
 import java.net.SocketException;
 
+import javax.swing.JLabel;
 import javax.swing.SwingWorker;
 
 import com.company.MainForm;
@@ -50,8 +51,8 @@ public class HandleDisconnectRequest extends SwingWorker<String, String> {
 	protected String doInBackground() throws Exception {
 		// TODO Auto-generated method stub
 		if(mainForm !=null && clientInfo !=null && offerAddress!=null) {
-			if(clientInfo.getClientName() == mainForm.getConnectedDeviceName()
-					&& offerAddress == InetAddress.getByName(mainForm.getConnectedDeviceAdress())) {
+			if(clientInfo.getClientName().equals(mainForm.getConnectedDeviceName())
+					&& offerAddress.getHostAddress().equals(mainForm.getConnectedDeviceAdress())) {
 				clearConnectedDevice(mainForm);
 			}
 		}
@@ -63,6 +64,11 @@ public class HandleDisconnectRequest extends SwingWorker<String, String> {
 		mForm.setConnectedDeviceAdress(CommonConstant.NOT_AVAIABLE);
 		mForm.setConnectedDeviceName(CommonConstant.NOT_AVAIABLE);
 		mForm.setDeviceConnected(false);
+		JLabel offerAdressLabel = (JLabel) mForm.getComponentByName("dAddrOutput");
+		JLabel offerNameLabel = (JLabel) mForm.getComponentByName("dNameOutput");
+		offerAdressLabel.setText(mForm.getConnectedDeviceAdress());
+		offerNameLabel.setText(mForm.getConnectedDeviceName());
+		
 		try {
 			MainForm.getdSocket().setSoTimeout(0);
 		} catch (SocketException e) {
